e7233f24e60401673374f5a926d5f1c0a628de7f
Nathan Scott <nscott3@sheffield.ac.uk>
2024-03-18 15:13:20 +0000

    create pub key file

--- ecdsa_certgen.sh
+++ ecdsa_certgen.sh
@@ -2,3 +2,6 @@
 
 openssl req -new -sha384 -key secp384r1_privkey.pem -out secp384r1_sha_384_signreq.csr -subj "/C=CH/ST=Zurich/L=Zurich/O=ETH Interfocus Course/OU=IT/CN=www.example.com/emailAddress=benjamin.dowling@inf.ethz.ch"
 openssl x509 -req -days 365 -in secp384r1_sha_384_signreq.csr -signkey secp384r1_privkey.pem -out secp384r1_sha_384_certificate.pem
+
+openssl req -new -sha256 -key secp256k1_privkey.pem -out secp256k1_sha_256_signreq.csr -subj "/C=CH/ST=Zurich/L=Zurich/O=ETH Interfocus Course/OU=IT/CN=www.example.com/emailAddress=benjamin.dowling@inf.ethz.ch"
+openssl x509 -req -days 365 -in secp256k1_sha_256_signreq.csr -signkey secp256k1_privkey.pem -out secp256k1_sha_256_certificate.pem
